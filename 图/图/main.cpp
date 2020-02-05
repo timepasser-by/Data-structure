0d852ada1c9619c223836695c36c45aad0038206
timepasserby <931585794@qq.com>
2020-02-05 20:53:47 +0800

    图

---
+++ 图/图/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<unordered_map>
+#include"Graph.h"
+ 
+
+int main()
+{
+	vector<char>citys = {"A","B","C","D","E"};
+	char relations[][2] = {
+		{"A","C"},
+		{"A","D"},
+		{"B","C"},
+		{"C","D"},
+		{"D","E"}
+	};
+	return 0;
+}
